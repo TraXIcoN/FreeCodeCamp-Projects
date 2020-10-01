68c7897cb584df4505827cf7a58b83494abcbf99
Priyansh2001here <57267750+Priyansh2001here@users.noreply.github.com>
2020-10-01 19:52:35 +0530

    Update bubble_sort.py
    added typing and used documentation

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,11 +1,27 @@
 
-def bubblesort(a):
-  n = len(a)
-  for i in range(n):
-    for j in range(n-1):
-      if a[j]>a[j+1]:
-        a[j],a[j+1] = a[j+1],a[j]
-  return a
+# Bubble Sort in Python
 
-a = list(map(int, input().strip().split(' ')))
-a = bubblesort(a)
+from typing import List
+
+def bubbleSort(arr : List) -> List: 
+    n = len(arr) 
+
+    sortedList = arr.copy()
+    # loop to traverse through all elements of list
+    for i in range(n): 
+
+        # Last i elements have already been sorted
+        for j in range(0, n-i-1): 
+
+            # if elemrnt is found greater then swap those
+            if sortedList[j] > sortedList[j+1] : 
+                sortedList[j], sortedList[j+1] = sortedList[j+1], sortedList[j] 
+
+    return sortedList
+
+
+if __name__ == '__main__':
+  a = list(map(int, input().strip().split(' ')))
+  a = bubblesort(a)
+  print(a)
+
