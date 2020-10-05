c68817c16e3691b24de696272e3e52e9ee8afd3a
Polina Shneider <beatrixlinkoln@gmail.com>
2020-10-06 01:51:13 +0300

    BubbleSort Task

---
+++ Javascript/BubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
