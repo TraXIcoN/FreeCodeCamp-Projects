a1893f34f160b82b916c56659e812cc93af39931
saranshkotnala <66181092+saranshkotnala@users.noreply.github.com>
2020-10-17 14:03:01 +0530

    Django web app biddiing

--- Django/Bidding app/commerce/settings.py
+++ Django/Bidding app/commerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
