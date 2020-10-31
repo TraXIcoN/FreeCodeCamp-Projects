a800a5f144815b058fcebf17919a8830211852c3
bachkukkik <krichkorn@gmail.com>
2020-10-31 19:26:27 +0700

    add try-except cluase for sum.py

--- Python/PythonBasics/sum.py
+++ Python/PythonBasics/sum.py
@@ -1,12 +1,19 @@
 a = int(input("Enter 1st Number"))
 b = int(input("Enter 2st Number"))
-c = a + b
 
-if c <= 20 :
-  print("no operation performed")
-elif c > 20 and c <= 40 :
-  print("product is ",(a*b))
-elif c > 40 and c <= 60 :
-  print("difference is ",(a-b))
-else :
-  print("sum is ",c)
+try:
+  ## make sure if a, b are number
+  for e in [a, b]:
+    assert type(e) in [int, float]
+  c = a + b
+
+  if c <= 20 :
+    print("no operation performed")
+  elif c > 20 and c <= 40 :
+    print("product is ",(a*b))
+  elif c > 40 and c <= 60 :
+    print("difference is ",(a-b))
+  else :
+    print("sum is ",c)
+except:
+  print("invalid input")
